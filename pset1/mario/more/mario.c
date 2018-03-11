010cb270e854112b2185b0cb76b03ff7a4baf272
Aditya Verma <adi004.zz@gmail.com>
2018-03-11 15:32:33 +0530

    Added relevant comments

--- pset1/mario/more/mario.c
+++ pset1/mario/more/mario.c
@@ -1,31 +1,33 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int main(void)
+//libraries header files
+
+int main(void) //main function
 {
 
-    int n = get_int("Height:");
-    int hashNumber = 1;
-    char hash = '#';
-    while (!(n >= 0 && n <= 23))
+    int n = get_int("Height:");  //getting height from user
+    int hashNumber = 1;         //initializing hash
+    char hash = '#';           // initializing the char
+    while (!(n >= 0 && n <= 23)) // checking for number in b/w 0 and 23
     {
         n = get_int("Height:");
     }
-    for (int i = n; i > 0; i--)
+    for (int i = n; i > 0; i--) // looping through
     {
-        printf("%*s", i - 1, "");
-        for (int j = 1; j <= hashNumber; j++)
+        printf("%*s", i - 1, ""); //adding space before
+        for (int j = 1; j <= hashNumber; j++) 
         {
-            printf("%c", hash);
+            printf("%c", hash); // printing hashes
         }
-        printf("%s", "  ");
+        printf("%s", "  "); //2 spaces in b/w
 
         for (int k = 1; k <= hashNumber; k++)
         {
-            printf("%c", hash);
+            printf("%c", hash); //printing hashes other side
         }
-        printf("%s\n", "");
-        hashNumber++;
+        printf("%s\n", ""); //prints spaces at the end
+        hashNumber++; //increment number by 1
 
     }
 }
